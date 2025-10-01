b273595176738e8ded4dd2d1d7d19b23eba5ac53
Victor Carmona <victorcarmona9035.com>
2025-10-01 12:53:17 +0200

    what are Tuples?

---
+++ app.py
@@ -0,0 +1,16 @@
+
+#Creacion de una tupla
+frutas =("apple","banana","cherry")
+print(frutas)
+
+#Imprimir ultimo elemento de una tupla
+print(frutas[-1])
+print(frutas[2])
+
+coordinates= (10,20,30)
+x,y,z = coordinates
+print(x)
+print(y)
+print(z)
+
+
