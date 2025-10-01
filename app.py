8be2f8a426aaa569d17b556679d40efd997afd58
Victor Carmona <victorcarmona9035.com>
2025-10-01 14:42:53 +0200

    Operaciones en conjuntos

--- app.py
+++ app.py
@@ -12,3 +12,20 @@ print(ingredients)
 #Eliminar un ingrediente
 ingredients.remove("flour")
 print(ingredients)
+
+#Los conjuntos no tendran nunca duplicados
+##Operaciones
+conjunto_a = {"flour","sugar","butter"}
+conjunto_b = {"sugar","eggs"}
+
+#Unir dos conjuntos
+print(conjunto_a | conjunto_b)  # {'eggs', 'flour', 'sugar', 'butter'}
+
+#interseccion de dos conjuntos
+print(conjunto_a & conjunto_b)  #{'sugar'}
+
+#diferencia entre dos conjuntos
+print(conjunto_a-conjunto_b)  #{'butter', 'flour'}
+
+
+
